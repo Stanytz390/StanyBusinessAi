b79e14c2725d4ef964d66857f5389747bd45b05d
Stany Max Hub <stanytz953@gmail.com>
2026-07-26 03:12:13 +0300

    Update models.js

--- models.js
+++ models.js
@@ -2,17 +2,13 @@ import mongoose from 'mongoose';
 
 const OwnerSchema = new mongoose.Schema({
     ownerName: { type: String, required: true },
-    ownerNumber: { type: String, required: true, unique: true }, // Mfano: 255712345678
-    
-    // Mipangilio binafsi ya biashara yake
+    ownerNumber: { type: String, required: true, unique: true },
     businessName: { type: String, default: "Stany Max Hub Business" },
     welcomeMessage: { type: String, default: "Karibu kwenye kituo chetu cha huduma!" },
-    
-    // Orodha ya huduma za huyu mtu pekee
     services: [
         {
-            keyword: { type: String, required: true }, // Mfano: "1"
-            name: { type: String, required: true },    // Mfano: "Graphics Design"
+            keyword: { type: String, required: true },
+            name: { type: String, required: true },
             description: { type: String, required: true },
             price: { type: String, required: true },
             imageUrl: { type: String, default: "" }
